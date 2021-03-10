c6848f9dd7298c5b0ff1de4929e6d0baf9a85595
Jeffrey Chan <jachan.santiago@gmail.com>
2021-03-10 18:25:39 -0400

    update python packaging

--- setup.py
+++ setup.py
@@ -1,84 +1,27 @@
 import setuptools
-#from cmake_setuptools import CMakeExtension, CMakeBuildExt
 
-import os
-import subprocess
-import shutil
-import sys
-from setuptools import Extension
-from setuptools.command.build_ext import build_ext
-
-CMAKE_EXE = os.environ.get('CMAKE_EXE', shutil.which('cmake'))
+from skbuild import setup
 
 
-def check_for_cmake():
-    if not CMAKE_EXE:
-        print('cmake executable not found. '
-              'Set CMAKE_EXE environment or update your path')
-        sys.exit(1)
-
-
-class CMakeExtension(Extension):
-    """
-    setuptools.Extension for cmake
-    """
-
-    def __init__(self, name, sourcedir=''):
-        check_for_cmake()
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuildExt(build_ext):
-    """
-    setuptools build_exit which builds using cmake & make
-    You can add cmake args with the CMAKE_COMMON_VARIABLES environment variable
-    """
-
-    def build_extension(self, ext):
-        check_for_cmake()
-        if isinstance(ext, CMakeExtension):
-            output_dir = os.path.abspath(
-                os.path.dirname(self.get_ext_fullpath(ext.name)))
-
-            build_type = 'Debug' if self.debug else 'Release'
-            cmake_args = [CMAKE_EXE,
-                          ext.sourcedir,
-                          "-DCMAKE_C_COMPILER=gcc",
-                          "-DCMAKE_CXX_COMPILER=g++",
-                          #'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + output_dir,
-                          '-DCMAKE_BUILD_TYPE=' + build_type]
-            cmake_args.extend(
-                [x for x in
-                 os.environ.get('CMAKE_COMMON_VARIABLES', '').split(' ')
-                 if x])
-            self.build_temp = ext.sourcedir
-            print(cmake_args, self.build_temp)
-            env = os.environ.copy()
-            if not os.path.exists(self.build_temp):
-                os.makedirs(self.build_temp)
-            subprocess.check_call(cmake_args,
-                                  cwd=self.build_temp,
-                                  env=env)
-            subprocess.check_call(['make', '-j4'],
-                                  cwd=self.build_temp,
-                                  env=env)
-            print()
-        else:
-            super().build_extension(ext)
-
+cmake_args = ["-DCMAKE_C_COMPILER=gcc", "-DCMAKE_CXX_COMPILER=g++","-DCMAKE_BUILD_TYPE=Release"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
-setuptools.setup(
+setup(
     name="apriltag",
     version="0.0.1",
-    packages=setuptools.find_namespace_packages(include=['python']),
+    packages=["apriltag"],
+    package_dir = {"":"python"},
+    cmake_args=cmake_args,
+    include_package_data=True,
+    cmake_install_dir="python/apriltag",
+    cmake_source_dir=".",
+    cmake_with_sdist=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    ext_modules=[CMakeExtension("apriltag")],
-    cmdclass={'build_ext': CMakeBuildExt}
+    zip_safe=False,
+    setup_requires=["cmake"]
+    )
